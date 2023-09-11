a7bb09c0755a0553d4f9cf0205b74be4ffb13e20
Prince Yadav <74665623+Gammaprince@users.noreply.github.com>
2023-09-11 14:15:46 +0530

    Create Print_Shortest_Path.java

---
+++ Graph/Shortest_Path/Print_Shortest_Path.java
@@ -0,0 +1,71 @@
+/*
+It's very simple 
+just use array of length V+1 , and remember where does it come from , remembering parent works 
+It's kind of Memoization
+0 1 2 3 4 5 6 7
+0 1 1 1 2 4 4 5
+
+you can see for 7 <-5 
+for 5 , 5 <- 4
+for 4 , 4 <- 2
+for 2 , 2 <- 1
+for 1 , 1 <- 1 , where you break because parent == itself 
+
+C++ code for the same by Striver
+This question is not on leetcode
+*/
+
+class Solution
+{
+	public:
+	  //Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Create a set ds for storing the nodes as a pair {dist,node}
+        // where dist is the distance from source to the node.
+        // set stores the nodes in ascending order of the distances 
+        set<pair<int,int>> st; 
+
+        // Initialising dist list with a large number to
+        // indicate the nodes are unvisited initially.
+        // This list contains distance from source to the nodes.
+        vector<int> dist(V, 1e9); 
+        
+        st.insert({0, S}); 
+
+        // Source initialised with dist=0
+        dist[S] = 0;
+        
+        // Now, erase the minimum distance node first from the set
+        // and traverse for all its adjacent nodes.
+        while(!st.empty()) {
+            auto it = *(st.begin()); 
+            int node = it.second; 
+            int dis = it.first; 
+            st.erase(it); 
+            
+            // Check for all adjacent nodes of the erased
+            // element whether the prev dist is larger than current or not.
+            for(auto it : adj[node]) {
+                int adjNode = it[0]; 
+                int edgW = it[1]; 
+                
+                if(dis + edgW < dist[adjNode]) {
+                    // erase if it was visited previously at 
+                    // a greater cost.
+                    if(dist[adjNode] != 1e9) 
+                        st.erase({dist[adjNode], adjNode}); 
+                        
+                    // If current distance is smaller,
+                    // push it into the queue
+                    dist[adjNode] = dis + edgW; 
+                    st.insert({dist[adjNode], adjNode}); 
+                 }
+            }
+        }
+        // Return the list containing shortest distances
+        // from source to all the nodes.
+        return dist; 
+    }
+};
